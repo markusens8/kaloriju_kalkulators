18e7ef32736f335c70bebfbec5bb2ec8285905f0
markusens8 <markuss.kirilovics@valmierastehnikums.lv>
2026-01-16 10:59:20 +0200

    Statiskais savadits, dati ieguti JS

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,8 @@ function izgutDatus() {
 }
 
 function aprekinat() {
-
+  const form = document.getElementById('dati');
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData);
+  console.log('form data as object', data);
 }
