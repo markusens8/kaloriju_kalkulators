18721a536ab0152f28d60922cacf84260a49b67a
markusens8 <markuss.kirilovics@valmierastehnikums.lv>
2026-01-19 10:47:40 +0200

    qtiri pri

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,23 @@
-function izgutDatus() {
-  
+function ieladetSadalu(sadala) {
+  document.getElementById("app").body.innerHTML = '';
+
+  let ievade = document.getElementById(sadala);
+  let klons = ievade.content.cloneNode(true);
+  document.body.appendChild(klons);
 }
 
-function aprekinat() {
-  const form = document.getElementById('dati');
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
-  console.log('form data as object', data);
+function aprekinatBMR() {
+  const dati = Object.fromEntries(new FormData('dati'));
+
+  let bmr = 10*dati.svars + 6.25*dati.garums - 5*dati.vecums
+  bmr += (dati.vecums == 'virietis' ? 5 : -161);
+  return bmr;
 }
+
+function paraditRezultatus() {
+  let bmr = aprekinatBMR;
+  ieladetSadalu('rezultats')
+}
+
+
+ieladetSadalu('ievade');
